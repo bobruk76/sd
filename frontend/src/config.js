4bfb9788781891a365591aa19703043b48791363
bobruk76 <vladimir.m.polyakov@gmail.com>
2021-11-16 17:04:47 +0300

    begin fix bugs

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,2 @@
-export const API_BASE_URL = 'https://vue-tzr.skillbox.cc/api';
+export const API_BASE_URL = 'https://vue-study.skillbox.cc/api';
+// export const API_BASE_URL = 'https://vue-tzr.skillbox.cc/api';
