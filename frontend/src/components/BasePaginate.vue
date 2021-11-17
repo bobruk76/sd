a15450f89e21b8d9424d0845e9974ab4250e894e
bobruk76 <vladimir.m.polyakov@gmail.com>
2021-11-17 14:47:36 +0300

    add Composition API

--- frontend/src/components/BasePaginate.vue
+++ frontend/src/components/BasePaginate.vue
@@ -4,7 +4,7 @@
           <a href="#" class="pagination__link pagination__link--arrow"
              :class="{'pagination__link--disabled' : page == 1}"
              aria-label="Предыдущая страница"
-             @click.prevent="paginate(page-1)">
+             @click.prevent="doPaginate(page-1)">
             <svg width="8" height="14" fill="currentColor">
               <use xlink:href="#icon-arrow-left"></use>
             </svg>
@@ -14,7 +14,7 @@
         <li class="pagination__item" v-for="item in countPages" :key="item">
           <a href="#" class="pagination__link"
              :class="{'pagination__link--current' : page == item}"
-             @click.prevent="paginate(item)">
+             @click.prevent="doPaginate(item)">
             {{ item }}
           </a>
         </li>
@@ -22,7 +22,7 @@
         <li class="pagination__item">
           <a href="#" class="pagination__link pagination__link--arrow"
              :class="{'pagination__link--disabled' : page == countPages}"
-             @click.prevent="paginate(page+1)"
+             @click.prevent="doPaginate(page+1)"
              aria-label="Следующая страница">
             <svg width="8" height="14" fill="currentColor">
               <use xlink:href="#icon-arrow-right"></use>
@@ -33,22 +33,25 @@
 </template>
 
 <script>
+import { computed } from 'vue';
+
 export default {
-  model: {
-    prop: 'page',
-    event: 'paginate',
-  },
 
-  props: ['page', 'countPages'],
+  props: ['modelValue', 'countPages'],
 
-  methods: {
-    paginate(page) {
-      if ((page >= 1) && (page <= this.countPages)) {
-        this.$emit('paginate', page);
+  setup(props, { emit }) {
+    const page = computed(() => props.modelValue);
+    const doPaginate = (newPage) => {
+      if ((newPage >= 1) && (newPage <= props.countPages)) {
+        emit('update:modelValue', newPage);
       }
-    },
+    };
+
+    return {
+      doPaginate,
+      page,
+    };
   },
-  computed: {
-  },
+
 };
 </script>
