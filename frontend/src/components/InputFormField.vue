ad297cfdfbb399bea8dc91666989ee0e9e9e3cb0
bobruk76 <vladimir.m.polyakov@gmail.com>
2021-12-07 15:19:50 +0300

    fix somethings

--- frontend/src/components/InputFormField.vue
+++ frontend/src/components/InputFormField.vue
@@ -1,14 +1,20 @@
 <template>
-  <BaseFormField :title="title" :placeholder="placeholder" :error="error">
+  <base-form-field :title="title" :placeholder="placeholder" :error="error">
     <input class="form__input"
-           v-model="dataValue" :type="type" :name="name" :placeholder="placeholder">
-  </BaseFormField>
+           v-model="dataValue"
+           :type="type"
+           :name="name"
+           :placeholder="placeholder"
+    >
+  </base-form-field>
 </template>
 
 <script>
 import formFieldMixin from '@/mixins/formFieldMixin';
+import BaseFormField from '@/components/BaseFormField.vue';
 
 export default {
+  components: { BaseFormField },
   mixins: [formFieldMixin],
   props: ['type'],
 };
--- frontend/src/components/TextareaFormField.vue
+++ frontend/src/components/TextareaFormField.vue
@@ -1,17 +1,21 @@
 <template>
-  <BaseFormField :title="title" :placeholder="placeholder" :error="error">
+  <base-form-field :title="title" :placeholder="placeholder" :error="error">
     <textarea class="form__input form__input--area"
-              v-model="dataValue" :name="name" :placeholder="placeholder"
+              v-model="dataValue"
+              :name="name"
+              :placeholder="placeholder"
     >
     </textarea>
     <span class="form__value">{{ title }}</span>
-  </BaseFormField>
+  </base-form-field>
 </template>
 
 <script>
 import formFieldMixin from '@/mixins/formFieldMixin';
+import BaseFormField from '@/components/BaseFormField.vue';
 
 export default {
+  components: { BaseFormField },
   mixins: [formFieldMixin],
 };
 </script>
